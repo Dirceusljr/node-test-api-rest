9f9a968ae0b166ce3af61cfbf2aec3dde2e9053f
Dirceusljr <dirceusljr@gmail.com>
2024-07-08 15:26:57 -0300

    aplicando os outros testes de login

--- src/test/routes/authRoutes.test.js
+++ src/test/routes/authRoutes.test.js
@@ -1,4 +1,7 @@
-import { afterEach, beforeEach, describe } from '@jest/globals';
+import {
+  afterEach, beforeEach, describe, expect,
+  it,
+} from '@jest/globals';
 import request from 'supertest';
 import app from '../../app';
 
@@ -24,5 +27,49 @@ describe('Testando a rota login (POST)', () => {
       .send(loginMock)
       .expect(500)
       .expect('"A senha de usuario é obrigatório."');
+  });
+
+  it('O login deve validar se o usuário está cadastrado', async () => {
+    const loginMock = {
+      email: 'dirceu.teste@teste.com.br',
+      senha: '123456',
+    };
+
+    await request(servidor)
+      .post('/login')
+      .set('Accept', 'application/json')
+      .send(loginMock)
+      .expect(500)
+      .expect('"Usuario não cadastrado."');
+  });
+
+  it('O login deve validar e-mail e senha incorreto', async () => {
+    const loginMock = {
+      email: 'dirceu@teste.com.br',
+      senha: '12345678',
+    };
+
+    await request(servidor)
+      .post('/login')
+      .set('Accept', 'application/json')
+      .send(loginMock)
+      .expect(500)
+      .expect('"Usuario ou senha invalido."');
+  });
+
+  it('O login deve validar se esta sendo retornado um accessToken', async () => {
+    const loginMock = {
+      email: 'dirceu@teste.com.br',
+      senha: '123456',
+    };
+
+    const resposta = await request(servidor)
+      .post('/login')
+      .set('Accept', 'application/json')
+      .send(loginMock)
+      .expect(201);
+
+    expect(resposta.body.message).toEqual('Usuario conectado');
+    expect(resposta.body).toHaveProperty('accessToken');
   });
 });
